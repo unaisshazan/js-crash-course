def85e8a5092178b52cb1a145cb7f5898f300966
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-22 17:58:02 +0500

    add

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//string operation
+let fruit ='banana,unais ,ali,mango';
+let moreFruits='banana\apple';
+
+console.log(fruit.length);
+console.log(fruit.indexOf('nan'));
+console.log(fruit.slice(2,6));
+console.log(fruit.replace('ban','123'));
+console.log(fruit.toUpperCase());
+console.log(fruit.charAt(2));
+console.log(fruit[2]);
+console.log(fruit.split(','))
+console.log(fruit.split(''))
